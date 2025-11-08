1eeaadd35d746931b2adea83bee5fb7991ad9a9d
Keagan Nguyen <55466279+nkhang505@users.noreply.github.com>
2025-11-07 16:18:36 -0800

    19 fix background on iphone (#23)
    * fix background / remove gold layer
    
    * remove motion in package.json:
    
    * incease size of close button in modal services, add backdrop click to close, add padding for gallery and randomized
    
    * make the gallery with zigzag pattern
    
    * add divider below gallery, remove unuse files
    
    * change image
    
    * fix image not showing
    
    * fix overflow hidden
    
    * fix center image

--- components/center-image.tsx
+++ components/center-image.tsx
@@ -5,8 +5,8 @@ const SECTION_HEIGHT = 1200;
 export const CenterImage = () => {
   const { scrollY } = useScroll();
 
-  const clip1 = useTransform(scrollY, [200, 600], [40, 0]);
-  const clip2 = useTransform(scrollY, [200, 600], [60, 100]);
+  const clip1 = useTransform(scrollY, [0, 1200], [40, 0]);
+  const clip2 = useTransform(scrollY, [0, 1200], [60, 100]);
 
   const clipPath = useMotionTemplate`polygon(${clip1}% ${clip1}%, ${clip2}% ${clip1}%, ${clip2}% ${clip2}%, ${clip1}% ${clip2}%)`;
 
@@ -23,15 +23,16 @@ export const CenterImage = () => {
 
   return (
     <motion.div
-      className="sticky top-0 w-full h-screen sm:h-screen md:h-100 max-w-1/2"
+      className="sticky top-0 h-screen w-full"
       style={{
         clipPath,
-        backgroundSize: "contain",        // Ensures the image covers the entire container
-        backgroundPosition: "center",    // Centers the image in the container
-        backgroundRepeat: "no-repeat",   // Prevents the image from repeating
+        backgroundSize,
         opacity,
-        backgroundImage: 'url("salon.jpg")',
+        backgroundImage:
+          "url(\"salon.JPG\")",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
       }}
-    ></motion.div>
+    />
   );
 };
